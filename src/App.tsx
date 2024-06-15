74a74eb2716154380fe0466179a40cc9f06c63ab
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-15 11:35:48 +0530

    added icons

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import { useQuery } from "react-query"
+import { Drawer, LinearProgress, Grid, Badge } from "@mui/material"
+import { AddShoppingCart } from "@mui/icons-material"
+
+function App() {
+
+  return (
+    <div>Start
+    </div>
+  )
+}
+
+export default App
