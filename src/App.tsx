ea9e6b115a704665e90bcdc1e69bbecfe0fee084
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-15 17:01:03 +0530

    chore: Add styled components for App layout and button

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import { useState } from "react"
 import { useQuery } from "react-query"
 import { Drawer, LinearProgress, Grid, Badge } from "@mui/material"
 import { AddShoppingCart } from "@mui/icons-material"
+import { Wrapper } from "./App.styles"
+
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number,
+}
 
 function App() {
+
+  const getProduct = async (): Promise<CartItemType> => {
+    return(
+    await (await fetch("https://fakestoreapi.com/products")).json()
+  )}
 
   return (
     <div>Start
