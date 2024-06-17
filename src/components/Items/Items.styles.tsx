969b6780efa474a19208ad7ee9d6030a71cf5e6f
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-17 09:41:50 +0530

    added button

--- src/components/Items/Items.styles.tsx
+++ src/components/Items/Items.styles.tsx
@@ -9,5 +9,13 @@ border: 1px solid lightblue;
 border-radius: 20px;
 height: 100%;
 
+button{
+    border-radius: 0 0 20px 20px;
+}
+img{
+    max-heigth: 250px;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+}
 
 `;
