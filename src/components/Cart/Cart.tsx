617670402c7a159e23d7f7f9b3f1a1266c72e6b6
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-17 09:40:22 +0530

    chore: Add type definitions and props to Cart component

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -2,7 +2,11 @@ import CartItem from "../CartItem/CartItem";
 import { Wrapper } from "./Cart.styles";
 import { CartItemType } from "../../App";
 
-
+type Props = {
+    cartItems : CartItemType[];
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+}
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
     const calculateTotal = (items: CartItemType[]) =>
