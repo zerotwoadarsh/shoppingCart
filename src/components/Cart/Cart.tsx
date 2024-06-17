9feb261abbe036efefa1b5d17a3f3ecf9c89f562
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-17 09:40:43 +0530

    cart updated

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -3,7 +3,7 @@ import { Wrapper } from "./Cart.styles";
 import { CartItemType } from "../../App";
 
 type Props = {
-    cartItems : CartItemType[];
+    cartItems: CartItemType[];
     addToCart: (clickedItem: CartItemType) => void;
     removeFromCart: (id: number) => void;
 }
@@ -11,8 +11,18 @@ type Props = {
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
     const calculateTotal = (items: CartItemType[]) =>
         items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
-    return(
-
+    return (
+        <Wrapper>
+            <h2>Your Shopping Cart</h2>
+            {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+            {cartItems.map(item => (<CartItem
+                key={item.id}
+                item={item}
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+            />))}
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+        </Wrapper>
     )
 }
 
